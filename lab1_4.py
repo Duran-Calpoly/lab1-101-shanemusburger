a9465f092b38bf21b54c0f4346f19a9ac1f07c83
shanemusburger <shanemusburger@gmail.com>
2026-01-15 00:36:35 +0000

    part 2 done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,21 @@
-pass
+def calculate_average(num1, num2, num3):
+    total = num1 + num2 + num3 
+    average= total/3
+    return average
+
+
+print(calculate_average (3,5,7))
+
+
+def add_tax(bill_total):
+    add_tax=bill_total*1.1
+    return add_tax
+
+print(add_tax(22))
+
+
+def greet_user(name):
+    greet_user="Hello " + name
+    return(greet_user)
+
+print(greet_user("bob")) 
